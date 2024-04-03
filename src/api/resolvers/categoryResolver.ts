74553508f9f51fed4fdbe33a8405feb369d52509
Liideli <roope.kasperi.laine@gmail.com>
2024-04-03 11:19:50 +0300

    category done

--- src/api/resolvers/categoryResolver.ts
+++ src/api/resolvers/categoryResolver.ts
@@ -8,6 +8,7 @@ const categoryData = [
     category_name: 'Mammal',
   },
 ];
+
 export default {
   Species: {
     category: (parent: Species) => {
@@ -25,12 +26,39 @@ export default {
     },
   },
   Mutation: {
-    addCategory: (
+    addCategory: async (
       _parent: undefined,
       args: {category: Omit<Category, '_id'>},
     ) => {
-      const newCategory = new categoryModel(args.category);
-      return newCategory.save();
+      const category = await categoryModel.create(args.category);
+      if (category) {
+        return {message: 'Category added', category};
+      } else {
+        return {message: 'Category not added'};
+      }
+    },
+    modifyCategory: async (
+      _parent: undefined,
+      args: {category: Omit<Category, '_id'>; id: string},
+    ): Promise<{message: string; category?: Category}> => {
+      const category = await categoryModel.findByIdAndUpdate(
+        args.id,
+        args.category,
+        {new: true},
+      );
+      if (category) {
+        return {message: 'Category updated', category};
+      } else {
+        return {message: 'Category not updated'};
+      }
+    },
+    deleteCategory: async (_parent: undefined, args: {id: string}) => {
+      const category = await categoryModel.findByIdAndDelete(args.id);
+      if (category) {
+        return {message: 'Category deleted', category};
+      } else {
+        return {message: 'Category not deleted'};
+      }
     },
   },
 };

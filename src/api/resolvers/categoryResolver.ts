b2d9f98e75a5d633be89f407779a5ef114621709
Liideli <roope.kasperi.laine@gmail.com>
2024-04-03 10:08:07 +0300

    first mutation

--- src/api/resolvers/categoryResolver.ts
+++ src/api/resolvers/categoryResolver.ts
@@ -1,19 +1,28 @@
-import {Species} from '../../types/DBTypes';
+import {Category, Species} from '../../types/DBTypes';
+import categoryModel from '../models/categoryModel';
 
+// TODO: categoryResolver
 const categoryData = [
   {
     id: '1',
     category_name: 'Mammal',
   },
 ];
-
 export default {
   Species: {
     category: (parent: Species) => {
-      console.log(parent);
       return categoryData.find(
         (category) => category.id === String(parent.category),
       );
+    },
+  },
+  Mutation: {
+    addCategory: (
+      _parent: undefined,
+      args: {category: Omit<Category, '_id'>},
+    ) => {
+      const newCategory = new categoryModel(args.category);
+      return newCategory.save();
     },
   },
 };
